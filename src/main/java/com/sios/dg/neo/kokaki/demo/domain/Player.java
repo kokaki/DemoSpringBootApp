39c33279446c57a89eea382b98cecfc7c97c6b52
t-kokaki <t-kokaki@sios.com>
2018-05-25 16:27:47 +0900

    C

--- src/main/java/com/sios/dg/neo/kokaki/demo/domain/Player.java
+++ src/main/java/com/sios/dg/neo/kokaki/demo/domain/Player.java
@@ -16,37 +16,37 @@ public class Player {
     private String position;
 
     public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public Integer getAge() {
-        return age;
-    }
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-    public String getTeam() {
-        return team;
-    }
-    public void setTeam(String team) {
-        this.team = team;
-    }
-    public String getPosition() {
-        return position;
-    }
-    public void setPosition(String position) {
-        this.position = position;
-    }
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getTeam() {
+		return team;
+	}
+	public void setTeam(String team) {
+		this.team = team;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
 
-    @Override
+	@Override
     public String toString() {
         return "Player [id=" + id + ", name=" + name + ", age=" + age + ", team=" + team + ", position=" + position + "]";
     }
